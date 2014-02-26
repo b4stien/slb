5a2e358341ba6d55b614f78fcb9a0700d33b2935
Benjamin Amelot <benjamin.amelot@student.ecp.fr>
2014-02-26 14:49:00 +0100

    Exemple de parcours des 3 pages web (home, searchPage, resultMats) avec Selenium

---
+++ tests/extracteur/Selenium_From_Home_To_ResultMats.py
@@ -0,0 +1,25 @@
+# goal : opening eQuality home page and clicking on MatsQuery to access search page
+
+# Selenium WebDriver modules
+from selenium import webdriver
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.common.exceptions import TimeoutException
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.support.ui import WebDriverWait
+import os
+
+myUrl = "file:///" + os.path.dirname(os.path.abspath(__file__)) + "/home.htm"
+
+firefox = webdriver.Firefox()
+firefox.get(myUrl)
+
+
+area = firefox.find_element_by_xpath("//area[contains(@alt, 'Query Performed Tests')]")
+
+test = ActionChains(firefox)
+test.click(area)
+test.perform()
+
+
+t = firefox.find_element_by_name("sabutton")
+t.submit()
